4f99b151f1281b63f14794f28b083716998549dc
Nilesh-Mudaliar <145035423+Nilesh-Mudaliar@users.noreply.github.com>
2025-01-29 03:18:32 +0530

    modified the routes: /checkout, /cart, /browse

---
+++ CC_Monolith-1/cart/__init__.py
@@ -0,0 +1,44 @@
+import json
+
+import products
+from cart import dao
+from products import Product
+
+
+class Cart:
+    def __init__(self, id: int, username: str, contents: list[Product], cost: float):
+        self.id = id
+        self.username = username
+        self.contents = contents
+        self.cost = cost
+
+    def load(data):
+        return Cart(data['id'], data['username'], data['contents'], data['cost'])
+
+
+def get_cart(username: str) -> list:
+    cart_details = dao.get_cart(username)
+    if not cart_details:
+        return []
+    
+    # Flatten the contents and evaluate once
+    product_ids = []
+    for cart_detail in cart_details:
+        contents = eval(cart_detail['contents'])  # Consider using ast.literal_eval for safety
+        product_ids.extend(contents)
+    
+    # Batch fetch products
+    return [products.get_product(pid) for pid in product_ids]
+
+
+def add_to_cart(username: str, product_id: int):
+    dao.add_to_cart(username, product_id)
+
+
+def remove_from_cart(username: str, product_id: int):
+    dao.remove_from_cart(username, product_id)
+
+def delete_cart(username: str):
+    dao.delete_cart(username)
+
+
